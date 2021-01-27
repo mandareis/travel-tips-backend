b837393372b85fcc6e9172c1ccd34b5942b1c72b
Amanda De Paula <amanda@depaula.xyz>
2021-01-27 14:08:00 -0800

    minor fixes to user's controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     end
     result = user.change_password(params[:new_password])
     if not result 
-      render json: {:error => user.errors.full_messages }, status: 400
+      render json: {:error => user.errors.full_messages[0] }, status: 400
       return
     end
     render json: user, except: [:password_digest]
