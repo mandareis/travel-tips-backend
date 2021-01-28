517f5e9a37023c56dbadcba6dd1e3d5f0d0378b5
Amanda De Paula <amanda@depaula.xyz>
2021-01-28 13:41:53 -0800

    fixes validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  PASSWORD_REQUIREMENTS =/\A
+  PASSWORD_REQUIREMENTS = /\A
   (?=.{6,})
   (?=.*\d)
   (?=.*[a-z])
@@ -12,10 +12,10 @@ class User < ApplicationRecord
   has_many :likes, through: :comments
   validates :username, :email, uniqueness: true
   validates :password, format: { with: PASSWORD_REQUIREMENTS, message: "must be minimum of 6 characters. 
-  Contain an  uppercase letter. 
+  Contain an  uppercase letter. Contain a lowercase letter.
   Contain a number." }, on: [:change_password, :create]
-  validates :email, format: { with: /\A[^@\s]+@[^@\s]+[a-zA-Z0-9]+\z/ }  
-  validates :username, format: {with: /\A[a-zA-Z0-9]+\z/ } 
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+[a-zA-Z0-9]+\z/ }
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
   def change_password(password)
     with_transaction_returning_status do
